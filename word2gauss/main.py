6585ff3a59d93b15a3a591e910b1effab2c7f294
davide <davide@pop-os.localdomain>
2020-12-16 13:46:05 +0000

    specify more cores

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -18,7 +18,7 @@ from utils import cosine
 ######################### SETTINGS ############################################
 
 # War & Peace (MWE = 0) vs Wikipedia (MWE = 1)
-MWE = 1
+MWE = 0
 
 # embedding properties
 dimension = 100
@@ -36,7 +36,7 @@ sigma_min = 0.7
 sigma_max = 1.5
 
 # training properties
-num_workers = 4
+num_workers = 10
 report_schedule = 100
 eta = 0.1 # learning rate : pass float for global learning rate (no min) or dict with keys mu,sigma,mu_min,sigma_min (local learning rate for each)
 Closs = 0.1 # regularization parameter in max-margin loss
