762cb0a79b029009f827fc5ead635900df3d359b
davide <davide@pop-os.localdomain>
2020-12-19 12:00:59 +0000

    continue trying to get loss to print on each iteration

--- word2gauss/embeddings.pyx
+++ word2gauss/embeddings.pyx
@@ -812,6 +812,7 @@ cdef class GaussianEmbedding:
         '''
         Update the model with a single batch of pairs
         '''
+        cdef float x
         with nogil:
             x = train_batch(&pairs[0, 0], pairs.shape[0],
                         self.energy_func, self.gradient_func,
