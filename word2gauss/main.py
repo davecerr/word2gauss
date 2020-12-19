17e491b0b4cba26a79d522b60fd8f8e54bccad7e
davide <davide@pop-os.localdomain>
2020-12-19 12:58:19 +0000

    continue trying to get loss to print on each iteration

--- word2gauss/main.py
+++ word2gauss/main.py
@@ -38,7 +38,7 @@ sigma_min = 0.7
 sigma_max = 1.5
 
 # training properties
-num_workers = 2
+num_workers = 10
 num_epochs = 1
 iteration_verbose_flag = True
 eta = 0.1 # learning rate : pass float for global learning rate (no min) or dict with keys mu,sigma,mu_min,sigma_min (local learning rate for each)
