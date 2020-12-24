8fb8097a332be31ce4200e003cf2ae97dbe03abc
davide <davide@pop-os.localdomain>
2020-12-24 18:21:47 +0000

    print training pairs for inspection

--- word2gauss/embeddings.pyx
+++ word2gauss/embeddings.pyx
@@ -788,8 +788,7 @@ cdef class GaussianEmbedding:
                 if pairs is None:
                     # no more data
                     break
-                cdef int arr_shape = pairs.shape
-                printf(arr_shape)
+                printf(pairs.shape)
                 batch_loss = self.train_batch(pairs)
                 with lock:
                     processed[0] += 1
